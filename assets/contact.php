7f93d5e6980b3cfe81ccabffcfea495187a4d945
Benjamin Fistein <bfistein@gmail.com>
2020-09-12 18:00:49 +0200

    Update contact.php

--- assets/contact.php
+++ assets/contact.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contactformtestgg@gmail.com";
+        $recipient = "wpdotnet@iolevel.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
