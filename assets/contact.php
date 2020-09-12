46f5d30cb49b87ef45b6cf2536b0504507d0bf54
Jakub Míšek <jmisek@gmail.com>
2020-09-12 18:38:15 +0200

    sendgrid

--- assets/contact.php
+++ assets/contact.php
@@ -1,52 +1,63 @@
 <?php
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["massage"]);
+// Only process POST reqeusts.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($subject) OR empty($lname) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Please complete the form and try again.";
-            exit;
-        }
+    include_once "../vendor/autoload.php";
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "wpdotnet@iolevel.com";
+    // Get the form fields and remove whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r", "\n"), array(" ", " "), $name);
+    $fromemail = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
 
-        // Set the email subject.
-        $subject = "New contact from $name";
-
-        // Build the email content.
-        $email_content = "First Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
-
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
-
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+    // Check that data was sent to the mailer.
+    if (empty($name) or empty($message) or !filter_var($fromemail, FILTER_VALIDATE_EMAIL)) {
+        // Set a 400 (bad request) response code and exit.
+        http_response_code(400);
+        echo "Please complete the form and try again.";
+        exit;
     }
 
-?>
+    $email = new SendGrid\Email();
 
+    // set subject
+    $email->setSubject("WpDotNet Contact from $name");
+
+    // attach the body of the email
+    $email->setFrom($fromemail);
+    $email->setFromName($name);
+    $email->addTo('wpdotnet@iolevel.com');
+    $email->setText("$message
+    
+---
+From: $name <$fromemail>
+    ");
+    
+    // account credentials
+    $username = getenv("SENDGRID_UNAME");
+    $password = getenv("SENDGRID_APIKEY");
+
+    // SendGrid object
+    $sendgrid = new SendGrid($username, $password);
+
+    try
+    {
+        // send message
+        $response = $sendgrid->send($email);
+
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    }
+    catch (\Exception $ex)
+    {
+        // Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+}
+else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
